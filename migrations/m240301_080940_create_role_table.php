8598872e8eac809581ec1035dc8266d683e0a42e
Z1n <tv.noob@bk.ru>
2024-03-01 17:22:23 +0700

    базовая структура

---
+++ migrations/m240301_080940_create_role_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%role}}`.
+ */
+class m240301_080940_create_role_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%role}}', [
+            'id' => $this->primaryKey(),
+            'code' => $this->string()->notNull()->unique(),
+            'name' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%role}}');
+    }
+}
---
+++ migrations/m240301_081352_create_status_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%status}}`.
+ */
+class m240301_081352_create_status_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%status}}', [
+            'id' => $this->primaryKey(),
+            'code' => $this->string()->notNull()->unique(),
+            'name' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%status}}');
+    }
+}
